ece537b3bcc996acf314f65d033357a910f9ebb1
Pierre Fernique <pfernique@gmail.com>
2016-06-10 14:00:15 +0200

    Initialize repository

---
+++ setup.py
@@ -0,0 +1,41 @@
+##################################################################################
+#                                                                                #
+# StatisKit-GLMCopyright (c) 2016 Pierre Fernique                                #
+#                                                                                #
+# This software is distributed under the CeCILL-C license. You should have       #
+# received a copy of the legalcode along with this work. If not, see             #
+# <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html>.                 #
+#                                                                                #
+# File authors: Pierre Fernique <pfernique@gmail.com> (1)                        #
+#                                                                                #
+##################################################################################
+
+\
+import os
+from setuptools import setup, find_packages
+
+packages = {"" : "src" + os.sep + "py"}
+for package in find_packages("src" + os.sep + "py"):
+    packages[package] = "src" + os.sep + "py"
+
+try:
+    from mngit.config import load_config
+    config = load_config('.')
+except:
+    import os
+    import yaml
+    with open('.' + os.sep + '.mngit.yml', 'r') as filehandler:
+        config = yaml.load(filehandler.read())
+
+with open('README.rst', 'r') as filehandler:
+    long_description = filehandler.read()
+
+setup(packages = packages.keys(),
+      package_dir = {"" : "src" + os.sep + "py"},
+      name = config['about']['name'],
+      version = config['about']['version'],
+      author = config['about']['authors'],
+      author_email = config['about']['email'],
+      description = config['about']['brief'],
+      long_description = long_description,
+      license = config['license']['plugin'])
