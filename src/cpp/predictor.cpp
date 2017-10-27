8893357e71a82aa5eb63b644912ab75a5bb126c6
Pierre Fernique <pfernique@gmail.com>
2017-10-27 10:27:31 +0200

    Update

--- src/cpp/predictor.cpp
+++ src/cpp/predictor.cpp
@@ -1,11 +1,3 @@
-/**********************************************************************************/
-/*                                                                                */
-/* StatisKit-CoreThis software is distributed under the CeCILL-C license. You     */
-/* should have received a copy of the legalcode along with this work. If not, see */
-/* <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html>.                 */
-/*                                                                                */
-/**********************************************************************************/
-
 #include "predictor.h"
 #include <eigen3/unsupported/Eigen/KroneckerProduct>
 
--- src/cpp/predictor.h
+++ src/cpp/predictor.h
@@ -1,11 +1,3 @@
-/**********************************************************************************/
-/*                                                                                */
-/* StatisKit-CoreThis software is distributed under the CeCILL-C license. You     */
-/* should have received a copy of the legalcode along with this work. If not, see */
-/* <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html>.                 */
-/*                                                                                */
-/**********************************************************************************/
-
 #ifndef STATISKIT_CORE_PREDICTOR_H
 #define STATISKIT_CORE_PREDICTOR_H
 
