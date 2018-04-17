cee0d3133eb7da37b11594772feee42742537f61
Pierre Fernique <pfernique@gmail.com>
2018-04-17 10:45:45 +0200

    Update

--- bin/conda/libstatiskit_glm-dev/build.sh
+++ bin/conda/libstatiskit_glm-dev/build.sh
@@ -1,6 +1,9 @@
 set -ve
 
-# scons autowig --prefix=$PREFIX -j$CPU_COUNT --autowig-no-wrappers
-scons cpp-dev
+if [[  ! ( "$(uname)" == "Linux" && "$PY3K" == "1" ) ]]; then
+   scons cpp-dev --prefix=$PREFIX -j$CPU_COUNT
+else
+    scons autowig --prefix=$PREFIX -j$CPU_COUNT --autowig-no-wrappers
+fi
 
 set +ve
